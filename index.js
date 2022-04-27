c109b991aadeb9b063af82d8adef1c20ebf31f81
makoks <kolesoks@gmail.com>
2022-04-28 03:07:48 +0500

    Вторая попытка решения

--- index.js
+++ index.js
@@ -7,30 +7,30 @@ function getUsefulInfo(letters) {
     let usefulInfo = null;
 
     if (letter.topic.match(/встреча/iu)) {
-      const matches = letter.message.match(/(0[1-9]|[12]\d|31)\.(0[1-9]|1[12])\.\d{4}\s([01]\d|2[1-3]):[0-5]\d/g);
+      const matches = letter.message.matchAll(/(?:^|[\s"(`{\['])((?:0[1-9]|[12]\d|31)\.(?:0[1-9]|1[12])\.\d{4}\s(?:[01]\d|2[1-3]):[0-5]\d)(?:$|[\s!`";:)}\]'.,])/g);
       if (matches) {
-        usefulInfo = matches;
+        usefulInfo = Array.from(matches).map(match => match[1]);
       }
     }
 
     if (letter.topic.match(/компания/iu)) {
-      const matches = letter.message.match(/[А-Я]+\s"[А-Яа-я\s]+"/u);
+      const matches = letter.message.match(/(?:^|[\s(`{\['])([А-Я]+\s"[^A-Za-z"]+")(?:$|[\s!`;:)}\]'.,])/u);
       if (matches) {
-        usefulInfo = matches[0];
+        usefulInfo = matches[1];
       }
     }
 
     if (letter.topic.match(/автомобиль/iu)) {
-      const matches = letter.message.match(/[АВЕКМНОРСТУХA-Z]\d{3}[АВЕКМНОРСТУХA-Z]{2}\s\d{2,3}/u);
+      const matches = letter.message.match(/(?:^|[\s"(`{\['])([АВЕКМНОРСТУХA-Z]\d{3}(?<!000)[АВЕКМНОРСТУХA-Z]{2}\s[1-9]\d{1,2})(?:$|[\s!`";:)}\]'.,])/u);
       if (matches) {
-        usefulInfo = matches[0];
+        usefulInfo = matches[1];
       }
     }
 
     if (letter.topic.match(/оплата/iu)) {
-      const matches = letter.message.match(/(\s|^)(\d{1,3}(,\d{3})*(.\d{2})?)\sр\./u);
+      const matches = letter.message.match(/(?:^|[\s"(`{\['])(\d{1,3}(?:,\d{3})*(?:.\d{2})?)\sр\.(?:$|[\s!`";:)}\]',])/u);
       if (matches) {
-        usefulInfo = Number(matches[2].replaceAll(',', ''));
+        usefulInfo = Number(matches[1].replaceAll(',', ''));
       }
     }
 
