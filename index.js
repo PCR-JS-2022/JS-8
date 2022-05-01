9f40f628e222db5e682d7d1dd02c20a8cac52986
makoks <kolesoks@gmail.com>
2022-05-01 20:37:37 +0500

    Пятая попытка решения 🤞

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function getUsefulInfo(letters) {
     let usefulInfo = null;
 
     if (letter.topic.match(/встреча/iu)) {
-      const matches = letter.message.matchAll(/(?:^|[\s"(`{\['])((?:0[1-9]|[12]\d|31)\.(?:0[1-9]|1[12])\.\d{4} (?:[01]\d|2[1-3]):[0-5]\d)(?:$|[\s!`";:)}\]'.,])/g);
+      const matches = letter.message.matchAll(/(?:^|[\s"(`{\['])((?:0[1-9]|[12]\d|3[01])\.(?:0[1-9]|1[0-2])\.\d{4} (?:[01]\d|2[0-3]):[0-5]\d)(?:$|[\s!`";:)}\]'.,])/g);
       if (matches) {
         const matchesArr = Array.from(matches);
         if (matchesArr.length) {
@@ -17,7 +17,7 @@ function getUsefulInfo(letters) {
     }
 
     if (letter.topic.match(/компания/iu)) {
-      const matches = letter.message.match(/(?:^|[\s(`{\['])((?:ИП|ООО|ОАО|АО|ЗАО) "[А-Яа-я" ]+")(?:$|[\s!`;:)}\]'.,])/u);
+      const matches = letter.message.match(/(?:^|[\s(`{\['])((?:ИП|ООО|ОАО|АО|ЗАО) "[А-Яа-яЁё" ]+")(?:$|[\s!`;:)}\]'.,])/u);
       if (matches) {
         usefulInfo = matches[1];
       }
