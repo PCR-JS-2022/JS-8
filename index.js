88aa24aef978a4d35567e9980352cb193690cfec
mmikriukov <>
2022-05-02 15:31:08 +0500

    first try

--- index.js
+++ index.js
@@ -3,8 +3,23 @@
  * @param {Array<{topic:string, message:string}>} letters 
  */
 function getUsefulInfo(letters) {
+  letters.forEach(letter => {
+    if (/встреча/gi.exec(letter.topic)) {
+      letter.usefulInfo = letter.message.match(/(0[1-9]|[12][0-9]|3[01])[.](0[1-9]|1[012])[.](((20)[0-9]{2})|(2100))\s(([01]?[0-9]|2[0-3]):[0-5][0-9]$)/gmi);
+    } else if (/компания/gi.exec(letter.topic)) {
+      letter.usefulInfo = letter.message.match(/(ИП|ООО|ОАО|АО|ЗАО)\s\".+?"/)[0].toString();
+    } else if (/автомобиль/gi.exec(letter.topic)) {
+      letter.usefulInfo = letter.message.match(/([A-ZА-Я]\d{3}[A-ZА-Я]{2}\s([0-9]{2,3}))+(?!\d)/)[0];
+    } else if (/оплата/gi.exec(letter.topic)) {
+      letter.usefulInfo = parseFloat(letter.message.match(/([\d\.,\s-]+[\d\.]+)/)[0].trim().replace(/,/g, ''));
+    } else {
+      letter.usefulInfo = null;
+    }
+  });
 
+  return letters;
 }
+
 
 module.exports = {
   getUsefulInfo,
