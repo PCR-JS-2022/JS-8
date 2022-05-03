31686027b3af7cd3b1a8d133222b8cf2740ddd94
deska13 <deskakuzmin1999@yandex.ru>
2022-05-03 14:32:59 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,27 +7,27 @@ function getUsefulInfo(letters) {
         let usefulInfo = null
         
         if (letter.topic.match(/встреча/i)) {
-            let res = letter.message.matchAll(/(?:^|[\s"(`{\['])((?:0[1-9]|[12]\d|3[01])\.(?:0[1-9]|1[0-2])\.\d{4} (?:[01]\d|2[0-3]):[0-5]\d)(?:$|[\s!`";:)}\]'.,])/g)
+            let res = letter.message.matchAll(/((0[1-9]|[12]\d|3[01])\.(0[1-9]|1[0-2])\.\d{4} ([01]\d|2[0-3]):[0-5]\d)/g)
             res = Array.from(res)
             usefulInfo = res.map((r) => r[1])
         }
 
         if (letter.topic.match(/компания/i)) {
-            let res = letter.message.match(/(?:^|[\s(`{\['])((?:ИП|ООО|ОАО|АО|ЗАО) "[а-яёa-z" ]+")(?:$|[\s!`;:)}\]'.,])/i) 
+            let res = letter.message.match(/((ИП|ООО|ОАО|АО|ЗАО) ".+")/i) 
             if (res) {
                 usefulInfo = res[1]
             }
         }
 
         if (letter.topic.match(/автомобиль/i)) {
-            let res = letter.message.match(/(?:^|[\s(`{\['])([А-Яа-яЁёA-Za-z]\d{3}(?<!000)[а-яёa-z]{2} \d{2,3})(?:$|[\s!`;:)}\]'.,])/i)
+            let res = letter.message.match(/([а-яёa-z]\d{3}(?<!000)[а-яёa-z]{2} \d{2,3})\b/i)
             if (res) {
                 usefulInfo = res[1]
             }
         }
         
         if (letter.topic.match(/оплата/i)) {
-            let res = letter.message.match(/(?:^|[\s(`{\['])((?:[1-9]\d{0,2}(?:,\d{3})*|0|[1-9]\d*)(?:\.\d{2})?) р\.(?:$|[\s!`";:)}\]',])/)
+            let res = letter.message.match(/((?:[1-9]\d{0,2}(?:,\d{3})*|0|[1-9]\d*)(?:\.\d{2})?) р\./)
             if (res) {
                 usefulInfo = Number(res[1].replaceAll(',', ''))
             }
