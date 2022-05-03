624847bcc71f8ebe40f37e50dbec9e97cd5516fa
mmikriukov <>
2022-05-03 15:53:21 +0500

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 function getUsefulInfo(letters) {
   letters.forEach(letter => {
     if (/встреча/gi.exec(letter.topic)) {
-      letter.usefulInfo = letter.message.match(/(?<!\d)((0[1-9]|[12][0-9]|3[01])[.](0[0-9]|1[012])[.](((20)[0-9]{2})|(2100))\s(([01][0-9]|2[0-3]):[0-5][0-9]$))/gmi);
+      letter.usefulInfo = letter.message.match(/(?<!\d)((0[1-9]|[12][0-9]|3[01])[.](0[0-9]|1[012])[.](([1-9])([0-9]{3}))\s(([01][0-9]|2[0-3]):[0-5][0-9]$))/gmi);
     } else if (/компания/gi.exec(letter.topic)) {
       letter.usefulInfo = letter.message.match(/(ИП|ООО|ОАО|АО|ЗАО)\s\".+?"/gmi) ? letter.message.match(/(ИП|ООО|ОАО|АО|ЗАО)\s\".+?"/gmi)[0].toString() : null;
     } else if (/автомобиль/gi.exec(letter.topic)) {
