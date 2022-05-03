7793d0a1b92cb9a796416f529cf6beec03dae7d1
mmikriukov <>
2022-05-03 16:36:55 +0500

    fix

--- index.js
+++ index.js
@@ -5,18 +5,18 @@
 function getUsefulInfo(letters) {
   letters.forEach(letter => {
     if (/встреча/gi.exec(letter.topic)) {
-      letter.usefulInfo = letter.message.match(/(?<!\d)((0[1-9]|[12][0-9]|3[01])[.](0[0-9]|1[012])[.](([1-9])([0-9]{3}))\s(([01][0-9]|2[0-3]):[0-5][0-9]$))/gmi);
+      letter.usefulInfo = letter.message.match(/(0[1-9]|[12]\d|3[01])[.](0[1-9]|1[0-2])[.](\d{4})\s([01]\d|2[0-3]):([0-5]\d)\b/gmui);
     } else if (/компания/gi.exec(letter.topic)) {
-      letter.usefulInfo = letter.message.match(/(ИП|ООО|ОАО|АО|ЗАО)\s\".+?"/gmi)
-        ? letter.message.match(/(ИП|ООО|ОАО|АО|ЗАО)\s\".+?"/gmi)[0].toString()
+      letter.usefulInfo = letter.message.match(/(ИП|ООО|ОАО|АО|ЗАО)\s["].+["]/mui)
+        ? letter.message.match(/(ИП|ООО|ОАО|АО|ЗАО)\s["].+["]/mui)[0]
         : null;
     } else if (/автомобиль/gi.exec(letter.topic)) {
-      letter.usefulInfo = letter.message.match(/([АВЕКМНОРСТУХA-Z]\d{3}(?<!000)[АВЕКМНОРСТУХA-Z]{2}\s([0-9]{2,3}\b))/gmi)
-        ? letter.message.match(/([АВЕКМНОРСТУХA-Z]\d{3}(?<!000)[АВЕКМНОРСТУХA-Z]{2}\s([0-9]{2,3}\b))/gmi)[0]
+      letter.usefulInfo = letter.message.match(/([АВЕКМНОРСТУХA-Z]\d{3}(?<!000)[АВЕКМНОРСТУХA-Z]{2})\s\d{2,3}\b/mui)
+        ? letter.message.match(/([АВЕКМНОРСТУХA-Z]\d{3}(?<!000)[АВЕКМНОРСТУХA-Z]{2})\s\d{2,3}\b/mui)[0]
         : null;
     } else if (/оплата/gi.exec(letter.topic)) {
-      letter.usefulInfo = letter.message.match(/([\d\.,\s-]+[\d\.]+)/mi)
-        ? parseFloat(letter.message.match(/([\d\.,\s-]+[\d\.]+)/mi)[0].trim().replace(/,/g, ''))
+      letter.usefulInfo = letter.message.match(/(([1-9]\d{0,2}(,\d{3})*|0|[1-9]\d*)([.]\d{2})?) р\./u)
+        ? parseFloat(letter.message.match(/(([1-9]\d{0,2}(,\d{3})*|0|[1-9]\d*)([.]\d{2})?) р\./u)[0].trim().replace(/,/g, ''))
         : null;
     } else {
       letter.usefulInfo = null;
