7fd1d65e694d543f6a00144a25aa80ad6080e774
deska13 <deskakuzmin1999@yandex.ru>
2022-05-03 14:51:38 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function getUsefulInfo(letters) {
         }
 
         if (letter.topic.match(/компания/i)) {
-            let res = letter.message.match(/((ИП|ООО|ОАО|АО|ЗАО) ".+")/i) 
+            let res = letter.message.match(/((ИП|ООО|ОАО|АО|ЗАО) ".+")/m) 
             if (res) {
                 usefulInfo = res[1]
             }
