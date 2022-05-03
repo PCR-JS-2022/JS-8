b494034c324caf2ef44162a7515913b04646c4fc
Grekova Elena <grekovalena01@gmail.com>
2022-05-03 19:07:13 +0500

    2

--- index.js
+++ index.js
@@ -7,24 +7,21 @@ function getUsefulInfo(letters) {
         let usefulInfo = null;
         if(letter.topic.match(/встреча/iu)) {
             const date = /(0[1-9]|[1-2]\d|3[0-1])\.(0[1-9]|1[012])\.\d{4} ([01]\d|2[0-3]):[0-5]\d/g;
-            usefulInfo = letter.message.match(date);
+            usefulInfo = letter.message.match(date) ? letter.message.match(date) : null;
         }
         if(letter.topic.match(/компания/i)) {
             const company = /(ИП|ООО|ОАО|АО|ЗАО) ".+"/g;
-            usefulInfo = letter.message.match(company)[0];
+            usefulInfo = letter.message.match(company) ? letter.message.match(company)[0] : null;
         }
         if(letter.topic.match(/автомобиль/i)) {
-            let auto = /[A-ZА-Я]\d{3}[A-ZА-Я]{2} \d{2,3}/g;
-            usefulInfo = letter.message.match(auto)[0];
+            let auto = /[A-ZАВЕКМНОРСТУХ]\d{3}[A-ZАВЕКМНОРСТУХ]{2} \d{2,3}/g;
+            usefulInfo = letter.message.match(auto) ? letter.message.match(auto)[0] : null;
         }
         if(letter.topic.match(/оплата/i)) {
             let payment = /(\d{1,3})(,\d{1,3})*(.\d{2})* р./g;
-            usefulInfo = Number(
-                letter.message
-                .match(payment)[0]
-                .slice(0, -3)
-                .replaceAll(',', '')
-            );
+            usefulInfo = letter.message.match(payment)
+                ? Number(letter.message.match(payment)[0].slice(0, -3).replaceAll(',', ''))
+                : null
         }
         return {
             topic: letter.topic,
