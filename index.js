b35ab9fc7e868be3f8f3f0c86bc51f748ed2d24c
thefirst24 <thefirst24@yandex.ru>
2022-05-03 13:32:17 +0500

    Первая попытка

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const PAYMENT = 'оплата';
 
 const keywords = [MEETING, COMPANY, CAR, PAYMENT];
 const keywordsRegex = {
-  [MEETING]: /[0-3][1-9].[0-1][1-9]\s[0-2][0-9]:[0][0-9]/,
+  [MEETING]: /[0-3][1-9].[0-1][1-9].[0-9]{4}\s[0-2][0-9]:[0-5][0-9]/g,
   [COMPANY]: /(ИП|ООО|ООО|ОАО|АО|ЗАО)\s"[ЁёА-я\s]+"/,
   [CAR]: /[\s.,][a-zA-ZЁёА-я][0-9]{3}[a-zA-ZЁёА-я]{2}\s[0-9]{2,3}[\s,.]/,
-  [PAYMENT]: //
+  [PAYMENT]: /[0-9]{1,3}(?:,?[0-9]{3})*(?:\.?[0-9]{2})/
 }
 
 /**
@@ -20,31 +20,119 @@ function getUsefulInfo(letters) {
     throw new TypeError('letters must be an array');
   }
 
+  const newLetters = [];
+
   for (const letter of letters) {
     let usefulInfo = null;
-    const regex = topicContainskeyword(letter.topic);
-    if () {
-      usefulInfo = [];
-      const index = letter.message.search(/[]/);
+    const keyword = topicContainskeyword(letter.topic);
+    switch (keyword) {
+      case MEETING:
+        usefulInfo = getMeetingDates(letter.message);
+        break;
+      case COMPANY:
+        usefulInfo = getFirstMatchedSubstring(letter.message, keywordsRegex[COMPANY]);
+        break;
+      case CAR:
+        usefulInfo = getCarNumber(letter.message);
+        break;
+      case PAYMENT:
+        usefulInfo = getFirstMatchedSubstring(letter.message, keywordsRegex[PAYMENT]);
+        if (usefulInfo) {
+          const removeCommas = usefulInfo.replace(/,/g, '');
+          usefulInfo = parseFloat(removeCommas);
+        }
+        break;
     }
+
+    const newLetter = {...letter, usefulInfo};
+    newLetters.push(newLetter);
   }
+
+  return newLetters;
 }
 
 /**
  * @param {string} topic
- * @return {RegExp | null}
+ * @return {string | null}
  */
 function topicContainskeyword(topic) {
   for (const keyword of keywords) {
     if (topic.includes(keyword)) {
-      return true;
+      return keyword;
     }
   }
 
   return null;
 }
 
+/**
+ * @param {string} message
+ * @return {Array<string> | null}
+ */
+function getMeetingDates(message) {
+  const regex = keywordsRegex[MEETING];
+  const matches = message.matchAll(regex);
+  let next = matches.next();
+  if (next?.done) {
+    return null;
+  }
+
+  const result = [];
+  while (!next.done) {
+    const date = next.value[0];
+    if (isDateValid(date)) {
+      result.push(date);
+    }
+    
+    next = matches.next();
+  }
+
+  return result;
+}
+
+/**
+ * @param {string} text
+ * @param {RegExp} regex
+ * @return {string | null}
+ */
+function getFirstMatchedSubstring(text, regex) {
+  const match = text.match(regex);
+  return match[0] || null;
+}
+
+/**
+ * @param {string} text
+ * @param {RegExp} regex
+ * @return {string | null}
+ */
+ function getCarNumber(text) {
+  const match = text.match(keywordsRegex[CAR]);
+  if (!match[0]) {
+    return null;
+  }
+
+  const fixingRegex = /[a-zA-ZЁёА-я][0-9]{3}[a-zA-ZЁёА-я]{2}\s[0-9]{2,3}/;
+
+  return match[0].match(fixingRegex)[0] || null;
+}
+
+/**
+ * @param {string} meetingDate
+ * @return {boolean}
+ */
+function isDateValid(meetingDate) {
+  const [date, time] = meetingDate.split(' ');
+  const [day, month] = date.split('.').map(el => parseInt(el));
+  if (day <= 0 || day > 31) { return false; }
+  if (month <= 0 || month > 12) { return false; }
+  if (day <= 0 || day > 31) { return false; }
+
+  const [hour, minute] = time.split(':').map(el => parseInt(el));
+  if (hour > 23 || minute > 59) { return false; }
+
+  return true;
+}
+
 module.exports = {
   getUsefulInfo,
 };
-
