0670315a43adde0562626385f4b433aa118b92fe
mmikriukov <>
2022-05-03 15:37:10 +0500

    again

--- index.js
+++ index.js
@@ -5,9 +5,9 @@
 function getUsefulInfo(letters) {
   letters.forEach(letter => {
     if (/встреча/gi.exec(letter.topic)) {
-      letter.usefulInfo = letter.message.match(/(?<!\d)((0[1-9]|[12][0-9]|3[01])[.](0[1-9]|1[012])[.](((20)[0-9]{2})|(2100))\s(([01]?[0-9]|2[0-3]):[0-5][0-9]$))/mi);
+      letter.usefulInfo = letter.message.match(/(?<!\d)((0[1-9]|[12][0-9]|3[01])[.](0[1-9]|1[012])[.](((20)[0-9]{2})|(2100))\s(([01]?[0-9]|2[0-3]):[0-5][0-9]$))/gmi);
     } else if (/компания/gi.exec(letter.topic)) {
-      letter.usefulInfo = letter.message.match(/(ИП|ООО|ОАО|АО|ЗАО)\s\".+?"/mi) ? letter.message.match(/(ИП|ООО|ОАО|АО|ЗАО)\s\".+?"/mi)[0].toString() : null;
+      letter.usefulInfo = letter.message.match(/(ИП|ООО|ОАО|АО|ЗАО)\s\".+?"/gmi) ? letter.message.match(/(ИП|ООО|ОАО|АО|ЗАО)\s\".+?"/gmi)[0].toString() : null;
     } else if (/автомобиль/gi.exec(letter.topic)) {
       letter.usefulInfo = letter.message.match(/([A-ZА-ЯЁ]\d{3}[A-ZА-ЯЁ]{2}\s([0-9]{2,3}))+(?!\d)/gmi) ? letter.message.match(/([A-ZА-ЯЁ]\d{3}[A-ZА-ЯЁ]{2}\s([0-9]{2,3}))+(?!\d)/mi)[0] : null;
     } else if (/оплата/gi.exec(letter.topic)) {
