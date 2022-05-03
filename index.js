6487d1cefd41e082e70bf40a11c0d75d2e92cd93
deska13 <deskakuzmin1999@yandex.ru>
2022-05-03 15:08:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function getUsefulInfo(letters) {
         }
 
         if (letter.topic.match(/автомобиль/i)) {
-            let res = letter.message.match(/([А-ЯЁA-Z]\d{3}(?<!000)[А-ЯЁA-Z]{2} \d{2,3})\b/m)
+            let res = letter.message.match(/(([А-ЯЁA-Z]\d{3}(?<!000)[А-ЯЁA-Z]{2}) (\d{2,3}))\b/m)
             if (res) {
                 usefulInfo = res[1]
             }
