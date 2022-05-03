84bd1c29de13698cbe0b4acca757a09bb3b69300
deska13 <deskakuzmin1999@yandex.ru>
2022-05-03 15:39:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function getUsefulInfo(letters) {
         let usefulInfo = null
         
         if (letter.topic.match(/встреча/iu)) {
-            let res = letter.message.match(/((0[1-9]|[12]\d|3[01])\.(0[1-9]|1[0-2])\.\d{4} ([01]\d|2[0-3]):([0-5]\d))/gmui)
+            let res = letter.message.match(/(0[1-9]|[12]\d|3[01])\.(0[1-9]|1[0-2])\.\d{4}\s([01]\d|2[0-3]):([0-5]\d)/gmui)
             if (res) {
                 res = Array.from(res)
                 if (res.length) {
