65a11d4a2a4627c44eaab8a67d67fb650de138ce
defimov9 <defimov9@gmail.com>
2022-05-04 02:13:39 +0500

    third

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ function getUsefulInfo(letters) {
       letter.usefulInfo = letter.message.match(/(0[1-9]|[12]\d|3[01])\.(0[1-9]|1[0-2])\.\d{4} ([01]\d|2[0-3]):([0-5]\d)/g);
     }
     if(letter.topic.match(/компания/ig)) {
-      letter.usefulInfo = letter.message.match(/(ИП|ООО|ОАО|АО|ЗАО) ".+"/)[0];
+      letter.usefulInfo = letter.message.match(/(ИП|ООО|ОАО|АО|ЗАО) ".+"/) ? letter.message.match(/(ИП|ООО|ОАО|АО|ЗАО) ".+"/)[0] : null;
     }
     if(letter.topic.match(/автомобиль/ig)) {
-      letter.usefulInfo = letter.message.match(/([АВЕКМНОРСТУХABEKMHOPCTYXA]\d{3}[АВЕКМНОРСТУХABEKMHOPCTYXA]{2}) (\d{2,3})/i)[0];
+      const matchCars = letter.message.match(/([АВЕКМНОРСТУХABEKMHOPCTYXA]\d{3}[АВЕКМНОРСТУХABEKMHOPCTYXA]{2}) (\d{2,3})/i);
+      letter.usefulInfo = matchCars ? matchCars[0] : matchCars;
     }
     if(letter.topic.match(/оплата/ig)) {
-      letter.usefulInfo = parseFloat(letter.message.match(/(\d{1,3},)*(\d{3})(\.\d{2})? [рp]\./)[0].split(" ")[0].replaceAll(",", ""));
+      const matchSum = letter.message.match(/(\d{1,3},)*(\d{3})(\.\d{2})? [рp]\./);
+      letter.usefulInfo = matchSum ? parseFloat(matchSum[0].split()[0].replaceAll(",", "")) : matchSum
     }
   });
   return letters;
