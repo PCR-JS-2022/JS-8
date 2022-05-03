12484bddf548583c1be0bc8a826e117788149764
mmikriukov <>
2022-05-03 16:46:37 +0500

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 function getUsefulInfo(letters) {
   letters.forEach(letter => {
     if (/встреча/gi.exec(letter.topic)) {
-      letter.usefulInfo = letter.message.match(/(0[1-9]|[12]\d|3[01])[.](0[1-9]|1[0-2])[.](\d{4})\s([01]\d|2[0-3]):([0-5]\d)\b/gmui);
+      letter.usefulInfo = letter.message.match(/((0[1-9]|[12][0-9]|3[01])[.](0[0-9]|1[012])[.](([1-9])([0-9]{3}))\s(([01][0-9]|2[0-3]):[0-5][0-9]$))/gmi);
     } else if (/компания/gi.exec(letter.topic)) {
       letter.usefulInfo = letter.message.match(/(?<![А-ЯЁа-яёA-Za-z0-9])(ИП|ООО|ОАО|АО|ЗАО)\s\".+"/mi)
         ? letter.message.match(/(?<![А-ЯЁа-яёA-Za-z0-9])(ИП|ООО|ОАО|АО|ЗАО)\s\".+"/mi)[0]
