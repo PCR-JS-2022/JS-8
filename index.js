3f9e9975cd6f535e2bc542b8825adc8f74643e09
Svetomuzika <sozonov_artem@mail.ru>
2022-05-04 15:30:49 +0500

    1

--- index.js
+++ index.js
@@ -4,9 +4,30 @@
  */
 function getUsefulInfo(letters) {
 
+    if (!Array.isArray(letters) || !letters) {
+        throw new Error("Невалидные данные");
+    }
+
+    letters.forEach(letter => {
+        if (letter.topic.match(/встреча/ig)) {
+            letter.usefulInfo = letter.message.match(/(0[1-9]|[12]\d|3[01])\.(0[1-9]|1[0-2])\.\d{4} ([01]\d|2[0-3]):([0-5]\d)/g);
+        }
+        if (letter.topic.match(/компания/ig)) {
+            const match = letter.message.match(/(ИП|ООО|ОАО|АО|ЗАО) ".+"/);
+            letter.usefulInfo = match ? match[0] : match;
+        }
+        if (letter.topic.match(/автомобиль/ig)) {
+            const matchCars = letter.message.match(/([АВЕКМНОРСТУХABEKMHOPCTYXA]\d{3}[АВЕКМНОРСТУХABEKMHOPCTYXA]{2}) (\d{2,3})/i);
+            letter.usefulInfo = matchCars ? matchCars[0] : matchCars;
+        }
+        if (letter.topic.match(/оплата/ig)) {
+            const match = letter.message.match(/(0|[1-9]\d*|[1-9]\d{0,2}(,\d{3})+)\.?(\d{2})? [pр]\./);
+            letter.usefulInfo = match ? parseFloat(match[0].replaceAll(",", "")) : match;
+        }
+    });
+    return letters;
 }
 
 module.exports = {
-  getUsefulInfo,
+    getUsefulInfo,
 };
-
