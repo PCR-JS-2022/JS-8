08b2a2a2076b90871745879fdcbe21ddfa93edac
Kisus228 <nik.babushkin.201408@gmail.com>
2022-06-09 02:13:59 +0500

    1-я попытка

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 /**
  * Функция находит и добавляет полезную информацию в письма
- * @param {Array<{topic:string, message:string}>} letters 
+ * @param {Array<{topic:string, message:string}>} letters
  */
 function getUsefulInfo(letters) {
+    const meetingRegExp = /встреча/gi;
+    const companyRegExp = /компания/gi;
+    const carRegExp = /автомобиль/gi;
 
+    const dateRegExp =
+        /(([0-2][0-9])|(3[0-1]))[.]((0[0-9])|(1[0-2]))[.]\d{4} (([0-1][0-9])|(2[0-3])):[0-5][0-9]/g;
+    const companyNameRegExp = /((ИП)|(ООО)|(ОАО)|(АО)|(ЗАО)) ".+?"/g;
+    const carPlateRegExp = /([УКЕНХВАРОСМТ])\d{3}[АОР]{2} \d{2,3}/g;
+
+    letters.forEach((letter) => {
+        const usefulInfo = [];
+        let result;
+        if (meetingRegExp.test(letter.topic)) {
+            while ((result = dateRegExp.exec(letter.message)) !== null) {
+                usefulInfo.push(result[0]);
+            }
+        } else if (companyRegExp.test(letter.topic)) {
+            while ((result = companyNameRegExp.exec(letter.message)) !== null) {
+                usefulInfo.push(result[0]);
+            }
+        } else if (carRegExp.test(letter.topic)) {
+            while ((result = carPlateRegExp.exec(letter.message)) !== null) {
+                usefulInfo.push(result[0]);
+            }
+        }
+
+        if (usefulInfo.length === 0) letter.usefulInfo = null;
+        else if (usefulInfo.length === 1) letter.usefulInfo = usefulInfo[0];
+        else letter.usefulInfo = usefulInfo;
+    });
+
+    return letters;
 }
 
 module.exports = {
-  getUsefulInfo,
+    getUsefulInfo,
 };
-
